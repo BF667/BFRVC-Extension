5b2720f14cbed6fdc6b887f95e5c247e89b0d5ab
BFNF <festyus09@gmail.com>
2025-06-07 23:45:06 +0700

    Create model_loader.py

---
+++ inference/model_loader.py
@@ -0,0 +1,22 @@
+import os
+
+
+rvc_models_dir = os.path.join(BASE_DIR, 'logs')
+
+
+
+def get_rvc_model(voice_model):
+    rvc_model_filename, rvc_index_filename = None, None
+    model_dir = os.path.join(rvc_models_dir, voice_model)
+    for file in os.listdir(model_dir):
+        ext = os.path.splitext(file)[1]
+        if ext == '.pth':
+            rvc_model_filename = file
+        if ext == '.index':
+            rvc_index_filename = file
+
+    if rvc_model_filename is None:
+        error_msg = f'No model file exists in {model_dir}.'
+        raise Exception(error_msg, is_webui)
+
+    return os.path.join(model_dir, rvc_model_filename), os.path.join(model_dir, rvc_index_filename) if rvc_index_filename else ''
